e8e0123fa453d8089f189347ef19ff1975de8968
Robin Rhodes <bigr0b@protonmail.com>
2022-07-03 23:10:33 -0400

    Add starter code for js part of calculator

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-const add = (a,b) => a + b;
-const subtract = (a,b) => a - b;
-const multiply =  (a,b) => a * b;
-const divide = (a,b) => a / b;
-
-const operate  = (operator, num1, num2) => {
-    return operator(num1, num2);
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+let input = "";
+const operate = (operator, num1, num2) => {
+  return operator(num1, num2);
+};
+const calcInput = document.getElementById("input");
+const calcDisplay = document.getElementById("result");
+const numBtns = document.getElementsByClassName("number");
+const clearBtn = document.querySelector(".clear");
+const equalBtn = document.querySelector(".calcSolve");
+const operatorBtn = document.querySelectorAll(".operator");
+// Number inputs
+calcInput.textContent = "";
+for (let i = 0; i < numBtns.length; i++) {
+  numBtns[i].addEventListener("click", function () {
+    calcInput.textContent += numBtns[i].value;
+    input += numBtns[i].value;
+  });
 }
 
-const calcDisplay = document.getElementById('displayNum');
-const numButtons = document.getElementsByClassName('calcNum');
+// Clear button
+clearBtn.addEventListener("click", function () {
+  calcInput.textContent = "";
+  calcDisplay.textContent = "";
+  input = "";
+});
 
-const zeroBtn = document.getElementById('zero')
+// Equal button
+// equalButton.addEventListener("click", function(){
+//     operate()
+// })
