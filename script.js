dcfd183fb067c5be503616b3fa7c1a84b7a1144b
Robin Rhodes <bigr0b@protonmail.com>
2022-07-03 23:44:29 -0400

    Add clearAll and equal Button functionality

--- script.js
+++ script.js
@@ -2,18 +2,32 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+let num1, num2, operator;
 let input = "";
-const operate = (operator, num1, num2) => {
-  return operator(num1, num2);
-};
+// switch (operator) {
+//   case "+":
+//     a + b;
+//     break;
+//   case "-":
+//     a - b;
+//     break;
+//   default:
+//     break;
+// }
 const calcInput = document.getElementById("input");
 const calcDisplay = document.getElementById("result");
 const numBtns = document.getElementsByClassName("number");
 const clearBtn = document.querySelector(".clear");
 const equalBtn = document.querySelector(".calcSolve");
 const operatorBtn = document.querySelectorAll(".operator");
+
+let clearAll = function () {
+  calcInput.textContent = "";
+  calcDisplay.textContent = "";
+  input = "";
+  operator = "";
+};
 // Number inputs
-calcInput.textContent = "";
 for (let i = 0; i < numBtns.length; i++) {
   numBtns[i].addEventListener("click", function () {
     calcInput.textContent += numBtns[i].value;
@@ -23,12 +37,10 @@ for (let i = 0; i < numBtns.length; i++) {
 
 // Clear button
 clearBtn.addEventListener("click", function () {
-  calcInput.textContent = "";
-  calcDisplay.textContent = "";
-  input = "";
+  clearAll();
 });
 
 // Equal button
-// equalButton.addEventListener("click", function(){
-//     operate()
-// })
+equalBtn.addEventListener("click", function () {
+  operate();
+});
